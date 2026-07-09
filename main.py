0ca9fb61bfcb7e2fe3a898b5f1104980a1e5c527
tristonfgibson <tristonfgibson@gmail.com>
2026-07-09 00:22:45 +0000

    Final code

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello")
+def fizz_or_buzz(value):
+    if value % 3 == 0:
+        print("fizz")
+
+    if value % 5 == 0:
+        print("buzz")
+
+    if value % 3 == 0 and value % 5 == 0:
+        print("fizzbuzz")
+
+
+for number in range(101):
+    
+    print(number)
+
+    fizz_or_buzz(number)
